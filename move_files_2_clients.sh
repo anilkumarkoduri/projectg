230e9f6fcce3b563c074e21ba9ade9e3052912a8
jasonbisson <jasonbisson@hotmail.com>
2017-08-10 02:15:09 +0000

    Commit number 2

--- move_files_2_clients.sh
+++ move_files_2_clients.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
+#set -x
 
 PATH="$PATH":/path/to/gsutil/
 TEMPFILE=/tmp/files
-gsutil ls gs://milkmoney/*.csv > $TEMPFILE
+BUCKET=milkmoney
+ICEBUCKET=milkmoneysvalbard
+gsutil ls gs://$BUCKET/*.csv > $TEMPFILE
 
 for x in `cat $TEMPFILE`
 do 
 FOLDER_SUFFIX=`echo $x | cut -b22-10000000 |cut -d '.' -f1`
 echo "Moving $x to client$FOLDER_SUFFIX folder"
-gsutil -m cp gs://milkmoney/client$FOLDER_SUFFIX.csv gs://milkmoneysvalbard/client$FOLDER_SUFFIX.csv
-gsutil -m mv gs://milkmoney/client$FOLDER_SUFFIX.csv gs://milkmoney/client$FOLDER_SUFFIX/client$FOLDER_SUFFIX.csv
+gsutil -m cp gs://$BUCKET/client$FOLDER_SUFFIX.csv gs://$ICEBUCKET/client$FOLDER_SUFFIX.csv
+gsutil -m mv gs://$BUCKET/client$FOLDER_SUFFIX.csv gs://$BUCKET/client$FOLDER_SUFFIX/client$FOLDER_SUFFIX.csv
 done
 
